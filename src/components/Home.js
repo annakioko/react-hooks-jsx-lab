306516bdababc02152ff23256596fd70811d8b7f
Anna Kioko <annakioko00@gmail.com>
2024-01-31 11:25:37 +0300

    completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,14 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
+  const name = "Anna";
+  const myCity = "Nairobi";
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">
+    <h1 style={{ color: 'redbrick' }}>
+      "{name} is a Web developer from {myCity}"
+    </h1>
+  </div>;
 }
 
 export default Home;
